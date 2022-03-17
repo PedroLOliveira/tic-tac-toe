2238b1bcf1787abd92e056809268f31adf431625
Pedro Lopes <letter.pedro@gmail.com>
2022-03-17 18:11:04 -0300

    commit inicial

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 93);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(93, 93);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 75);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(174, 93);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 75);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 174);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 75);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(93, 174);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 75);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(174, 174);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 75);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X Win Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "O Win Count";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Draw Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(203, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "0";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(93, 326);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "Reset";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 361);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button10;
+    }
+}
---
+++ JogoDaVelha/Form1.cs
@@ -0,0 +1,265 @@
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        bool aiTurn = false, button_disabled = false;
+        int humanWins = 0, aiWins = 0, draws = 0;
+        readonly string human = "X", ai = "O";
+        List<List<int>> winnerIndexes = new()
+        {
+            new List<int> { 0, 1, 2 },
+            new List<int> { 3, 4, 5 },
+            new List<int> { 6, 7, 8 },
+            new List<int> { 0, 3, 6 },
+            new List<int> { 1, 4, 7 },
+            new List<int> { 2, 5, 8 },
+            new List<int> { 0, 4, 8 },
+            new List<int> { 2, 4, 6 }
+        };
+
+        internal class Move
+        {
+            public int Points { get; set; }
+            public int Index { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LimpaJogo()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            button_disabled = false;
+            aiTurn = false;
+        }
+
+        private List<string> GetTable()
+        {
+            return new() { button1.Text, button2.Text, button3.Text, button4.Text, button5.Text, button6.Text, button7.Text, button8.Text, button9.Text };
+        }
+
+        private async void Play(Button button, string player)
+        {
+            button.Text = player;
+            if (aiTurn)
+            {
+                aiTurn = false;
+                button_disabled = false;
+            }
+            else
+            {
+                aiTurn = true;
+                button_disabled = false;
+            }
+            CheckForWinner();
+        }
+
+        private Move CalculateBetterMove(string player)
+        {
+            var table = GetTable();
+            var emptyCells = EmptyCells();
+
+            var move = new Move() { Index = -1, Points = -1000 };
+            for (var i = 0; i < emptyCells.Count; i++)
+            {
+                int score = MovementScore(emptyCells[i], player);
+
+                if (score > move.Points)
+                {
+                    move.Points = score;
+                    move.Index = emptyCells[i];
+                }
+            }
+            return move;
+        }
+
+        private int MovementScore(int index, string player)
+        {
+            // 02 pontos se for a posição do meio
+            // 01 ponto se for alguma das posições das bordas
+            // Menos 02 pontos, se já tiver uma peça do adversário na mesma linha, coluna ou diagonal da posição selecionada
+            // 04 pontos se a posição impedir a vitória do adversário
+            // 04 pontos se a jogada resultar em vitória
+            var table = GetTable();
+            int score = 0, center = 4;
+            int[] corners = { 0, 2, 6, 8 };
+            if (corners.Contains(index))
+                score += 1;
+            if (center == index)
+                score += 2;
+            foreach (var indexes in winnerIndexes.Where(i => i.Contains(index)))
+            {
+                if (CountPlayerInIndexes(table, player == human ? ai : human, indexes) == 1)
+                    score -= 2;
+                if (WinnerIndexes(table, player, indexes))
+                    score += 4;
+                if (WinnerIndexes(table, player == human ? ai : human, indexes))
+                    score += 4;
+            }
+            return score;
+        }
+
+        private void ProjectPlay(string player)
+        {
+            var betterMove = CalculateBetterMove(player);
+            Play(GetButtonByIndex(betterMove.Index), player);
+        }
+
+        private List<int> EmptyCells()
+        {
+            return GetTable().Select((value, index) => (value, index))
+                .Where(x => string.IsNullOrEmpty(x.value))
+                .Select(x => x.index).ToList();
+        }
+
+        private Button GetButtonByIndex(int index)
+        {
+            return index switch
+            {
+                0 => button1,
+                1 => button2,
+                2 => button3,
+                3 => button4,
+                4 => button5,
+                5 => button6,
+                6 => button7,
+                7 => button8,
+                8 => button9,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        private void CheckForWinner()
+        {
+            var table = GetTable();
+            if (Winner(table, human))
+            {
+                humanWins++;
+                label4.Text = Convert.ToString(humanWins);
+                MessageBox.Show("Humano venceu!");
+                button_disabled = true;
+            }
+            else if (Winner(table, ai))
+            {
+                aiWins++;
+                label5.Text = Convert.ToString(aiWins);
+                MessageBox.Show("Maquina venceu!");
+                button_disabled = true;
+            }
+            else if (!EmptyCells().Any())
+            {
+                draws++;
+                label6.Text = Convert.ToString(draws);
+                MessageBox.Show("Empate");
+                button_disabled = true;
+            }
+            if (aiTurn && !button_disabled)
+            {
+                ProjectPlay(ai);
+            }
+        }
+
+        private static bool Winner(List<string> table, string player)
+        {
+            if (table[0] == player && table[1] == player && table[2] == player || 
+                table[3] == player && table[4] == player && table[5] == player ||
+                table[6] == player && table[7] == player && table[8] == player ||
+                table[0] == player && table[3] == player && table[6] == player ||
+                table[1] == player && table[4] == player && table[7] == player ||
+                table[2] == player && table[5] == player && table[8] == player ||
+                table[0] == player && table[4] == player && table[8] == player ||
+                table[2] == player && table[4] == player && table[6] == player)
+                return true;
+
+            return false;
+        }
+
+        private static bool WinnerIndexes(List<string> table, string player, List<int> indexes)
+        {
+            if (table[indexes[0]] == player && table[indexes[1]] == player && table[indexes[2]] == player)
+                return true;
+            return false;
+        }
+
+        private static int CountPlayerInIndexes(List<string> table, string player, List<int> indexes)
+        {
+            int count = 0;
+            if (table[indexes[0]] == player)
+                count++;
+            if (table[indexes[1]] == player)
+                count++;
+            if (table[indexes[2]] == player)
+                count++;
+            return count;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button1.Text == "")
+                Play(button1, human);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button2.Text == "")
+                Play(button2, human);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button3.Text == "")
+                Play(button3, human);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button4.Text == "")
+                Play(button4, human);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button5.Text == "")
+                Play(button5, human);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button6.Text == "")
+                Play(button6, human);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button7.Text == "")
+                Play(button7, human);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button8.Text == "")
+                Play(button8, human);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (!aiTurn && !button_disabled && button9.Text == "")
+                Play(button9, human);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            LimpaJogo();
+        }
+    }
+}
